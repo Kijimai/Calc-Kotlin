6db8db31285f265bf4004896f3b6acf1f65359c8
John Deen Bucog <JDBucog@yahoo.com>
2023-01-10 19:38:51 -0800

    complete calc App, modified removeZeroAfterDecimal func to use endsWith instead of contains to prevent bugs like 1/100.

--- app/src/main/java/jibby/tutorials/calculatorapp/MainActivity.kt
+++ app/src/main/java/jibby/tutorials/calculatorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package jibby.tutorials.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.ArtistColumns
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -22,18 +23,113 @@ class MainActivity : AppCompatActivity() {
         tvInputDisplay?.append((view as Button).text)
         lastNumeric = true
         lastDot = false
-        Toast.makeText(this, "Button Clicked!", Toast.LENGTH_LONG).show()
     }
 
     fun onClear(view:View) {
-        tvInputDisplay?.text = "0"
+        tvInputDisplay?.text = ""
     }
 
     fun onDecimal(view: View) {
+        if(tvInputDisplay?.text.toString().contains(".")) {
+            return
+        }
         if (lastNumeric && !lastDot) {
             tvInputDisplay?.append(".")
             lastNumeric = false
             lastDot = true
         }
+    }
+
+    fun onOperator(view: View) {
+        // if tvInputDisplay exists and if it also has a text inside it -- execute the code below
+        tvInputDisplay?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInputDisplay?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if(value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
+    }
+
+    fun onEquals(view: View) {
+        if(lastNumeric) {
+            var tvValue = tvInputDisplay?.text.toString()
+            var prefix = ""
+            try {
+
+                if(tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0] // 99
+                    var two = splitValue[1] // 1
+
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    var result = (one.toDouble() - two.toDouble())
+                    tvInputDisplay?.text = removeZeroAfterDecimal(result.toString())
+                } else if(tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0] // 99
+                    var two = splitValue[1] // 1
+
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    var result = (one.toDouble() + two.toDouble())
+                    tvInputDisplay?.text = removeZeroAfterDecimal(result.toString())
+                } else if(tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0] // 99
+                    var two = splitValue[1] // 1
+
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    var result = (one.toDouble() / two.toDouble())
+                    tvInputDisplay?.text = removeZeroAfterDecimal(result.toString())
+                } else if(tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0] // 99
+                    var two = splitValue[1] // 1
+
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    var result = (one.toDouble() * two.toDouble())
+                    tvInputDisplay?.text = removeZeroAfterDecimal(result.toString())
+                }
+            } catch(e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDecimal(result: String): String {
+        var value = result
+        if(value.endsWith(".0")) {
+            value = result.substring(0, result.length-2)
+        }
+        return value
     }
 }
