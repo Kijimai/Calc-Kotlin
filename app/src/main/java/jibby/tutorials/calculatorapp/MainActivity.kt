4f5886d997f004cd7fee3d7064d336b17d12ef03
John Deen Bucog <JDBucog@yahoo.com>
2023-01-10 17:45:19 -0800

    setup layout, add onClick for digits, add onClear fun for clr btn

--- app/src/main/java/jibby/tutorials/calculatorapp/MainActivity.kt
+++ app/src/main/java/jibby/tutorials/calculatorapp/MainActivity.kt
@@ -2,10 +2,26 @@ package jibby.tutorials.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInputDisplay: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInputDisplay = findViewById(R.id.tvInputDisplay)
+    }
+
+    fun onDigit(view: View) {
+        tvInputDisplay?.append((view as Button).text)
+        Toast.makeText(this, "Button Clicked!", Toast.LENGTH_LONG).show()
+    }
+
+    fun onClear(view:View) {
+        tvInputDisplay?.text = "0"
     }
 }
